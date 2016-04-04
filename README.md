bf1c2eff527f8b86a6673abb878f6b605c7c40ae
horike37 <horike37@gmail.com>
2016-04-04 14:54:37 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# Social Stream Monitor
+## Description
+Twitterの特定ワードによる検索の結果をリアルタイムに可視化するツールです
+
+## Architecture
+
+
+
+# Setup
+## Twitter Stream to Kinesis
+### Step1 Install Python modules boto3 and TwitterAPI.
+
+    $ pip install boto3 TwitterAPI
+    
+### Step2 Edit twitter2kinesis.py
+
+Edit The Twitter API parameters
+
+    consumer_key = ""
+    consumer_secret = ""
+    access_token_key = ""
+    access_token_secret = ""
+    
+AWS parameters - from the Outputs tab of the CloudFormation template
+
+    access_key = ""
+    secret_access_key = ""
+    region = ""
+    stream_name = ""
+    
+### Execute
+
+    $ python twitter2kinesis.py
+    
+## Socket.io server
+### Step1 Install node module
+
+   $ npm install aws-sdk
+   $ npm install url
+   $ npm install async
+   $ npm install socket.io
+   
+### Step2 Edit app.js
+
+Amazon SNS parameters
+
+    var args = {
+        TopicArn:"arn:aws:sns:...",
+        Protocol:'http',
+        Endpoint:"http://<socket server host>:3000/httpsns"
+    };
+
+
+### Step3 Edit config/aws.json
+
+AWS parameters
+
+    {
+        "accessKeyId":"",
+        "secretAccessKey":"",
+        "region":""
+    }
+
---
+++ app.js
@@ -0,0 +1,85 @@
+var http = require('http');
+var url = require("url");
+var async = require('async');
+var AWS = require('aws-sdk');
+ 
+AWS.config.loadFromPath('./config/aws.json');
+var sns = new AWS.SNS();
+ 
+var httpServer = http.createServer(handler);
+var io = require('socket.io').listen(httpServer);
+var fs = require('fs');
+//asyncを使って順番に処理を実行
+async.series([
+    //HTTPサーバ起動
+    function (callback) {
+        httpServer.listen(3000);
+        callback(null, 1);
+    },
+    //subscrive開始
+    function (callback) {
+        initSubscriber(callback);
+        callback(null, 2);
+    }
+], function (err, results) {
+    if (err) {
+        throw err;
+    }
+});
+ 
+function handler(req, res) {
+    var path = url.parse(req.url).pathname;
+ 
+    if (path === "/httpsns") {
+        var body = '';
+        req.on('data', function (data) {
+            body += data;
+        });
+        req.on('end', function () {
+            res.writeHead(200, {
+                'Content-Type':'text/html'
+            });
+ 
+            var obj = JSON.parse(body);
+            if (obj.Type === "SubscriptionConfirmation") {
+                sns.confirmSubscription({ TopicArn:obj.TopicArn, Token:obj.Token}, function (err, data) {
+                    console.log("confirmSubscription");
+                });
+            } else if (obj.Type === "Notification" && obj.Message !== undefined) {
+                console.log(obj.Subject + ":" + obj.Message);
+    			io.sockets.emit('msg', obj.Message);
+            }
+            res.end('OK');
+        });
+    } else if ( path === "/dist/css/timeline.css" ) {
+    	res.writeHead(200, {'Content-Type':'text/css'});
+        res.end(fs.readFileSync('dist/css/timeline.css'));
+    } else if ( path === "/dist/css/bootstrap.min.css" ) {
+    	res.writeHead(200, {'Content-Type':'text/css'});
+        res.end(fs.readFileSync('dist/css/bootstrap.min.css'));
+    } else if ( path === "/dist/css/bootstrap.min.css.map" ) {
+    	res.writeHead(200, {'Content-Type':'text/css'});
+        res.end(fs.readFileSync('dist/css/bootstrap.min.css.map'));
+    } else if ( path === "/dist/css/sb-admin-2.css" ) {
+    	res.writeHead(200, {'Content-Type':'text/css'});
+        res.end(fs.readFileSync('dist/css/sb-admin-2.css'));
+    } else if ( path === "/data/worldmap.topojson" ) {
+    	res.writeHead(200, {'Content-Type':'application/json'});
+        res.end(fs.readFileSync('data/worldmap.topojson'));
+    } else {
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.end(fs.readFileSync('index.html'));
+    }
+}
+ 
+function initSubscriber(callback) {
+    var args = {
+        TopicArn:"",
+        Protocol:"",
+        Endpoint:""
+    };
+ 
+    sns.subscribe(args, function (err, data) {
+        console.log("subscribe start.");
+    });
+}
